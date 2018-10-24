72bad9ec1b416d3c872ffe4195c106a939ae8546
MDONNELLY-DT\Michael <michael.donnelly@solarwinds.com>
2018-10-24 16:28:09 +0100

    Added a new REST API test project
    Added a new REST API test project

---
+++ APIApplication/APIApplication/Features/APIApplication.feature
@@ -0,0 +1,8 @@
+﻿Feature: APIApplication
+	
+
+@mytag
+Scenario: Get API response using given endpoint
+	Given I have an endpoint /endpoint/
+	When I call get method of the API
+	Then I get the API response in json format
---
+++ APIApplication/APIApplication/Features/Steps/APIApplication.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace APIApplication.Features.Steps
+{
+    [Binding]
+    public sealed class APIApplication
+    {
+        [Given(@"I have an endpoint (.*)")]
+        public void GivenIHaveAnEndpointEndpoint(string endpoint)
+        {
+            RestApiHelper.SetUrl(endpoint);
+        }
+
+        [When(@"I call get method of the API")]
+        public void WhenICallGetMethodOfTheAPI()
+        {
+            RestApiHelper.CreateRequest();
+        }
+
+        [Then(@"I get the API response in json format")]
+        public void ThenIGetTheAPIResponseInJsonFormat()
+        {
+            var expected = "something";
+            var response = RestApiHelper.GetResponse();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Assert.That(response.Content, Is.EqualTo(expected), "Error Message");
+            }
+        }
+
+    }
+}
